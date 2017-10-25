ce68c9e596969b291299ad01856604bb59c5ad8e
Matt West <matt@codeslo.com>
2017-10-24 20:12:18 -0700

    fixed index and main

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
-    // $("#submit-button").click(function () {
-    //     $("input").each(function(){
-    //         var hasVal = $(this).val();
-    //         var fieldName = $(this).attr("data-fieldname");
-    //         if(!hasVal){
-    //             $(this).css("background-color","pink");
-    //             $(this).attr("placeholder",fieldName + " is required.");
-    //         }
-    //     });
-    // });
+    $("#submit-button").click(function () {
+        $("input").each(function(){
+            var hasVal = $(this).val();
+            var fieldName = $(this).attr("data-fieldname");
+            if(!hasVal){
+                $(this).css("background-color","pink");
+                $(this).attr("placeholder",fieldName + " is required.");
+            }
+        });
+    });
 
-    // $("input").click(function(){
-    //     var hasPlaceholder = $(this).attr("placeholder");
-    //     if(hasPlaceholder){
-    //         $(this).attr("placeholder","");
-    //         $(this).css("background-color","white");
-    //     }
-    // });
+    $("input").click(function(){
+        var hasPlaceholder = $(this).attr("placeholder");
+        if(hasPlaceholder){
+            $(this).attr("placeholder","");
+            $(this).css("background-color","white");
+        }
+    });
 
 });
