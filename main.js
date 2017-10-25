0803f2fa868256992a310f222a2bab31caf7bd9f
Matt West <matt@codeslo.com>
2017-10-25 14:03:06 -0700

    complete for class two

--- main.js
+++ main.js
@@ -1,54 +1,106 @@
 $(document).ready(function () {
     $("#submit-button").click(function () {
-        $(".validate-this").each(function(){
+        // valid must remain true for submit to complete. Any error should change it to false.
+        var valid = true;
+        $(".validate-this").each(function () {
             var hasVal = $(this).val();
             var fieldName = $(this).attr("data-fieldname");
-            if(!hasVal){
-                $(this).css("background-color","pink");
-                $(this).attr("placeholder",fieldName + " is required.");
-            }else{
-                isValid($(this).attr("name"));
+            if (!hasVal) {
+                valid = false;
+                $(this).css("background-color", "pink");
+                $(this).attr("placeholder", fieldName + " is required.");
+            } else {
+                if (!isValid($(this).attr("name"))) {
+                    valid = false;
+                }
             }
         });
+
+        if(valid){
+            // if valid is true after all checks, submit form
+            savePartier();
+        }
     });
 
-    $("input").click(function(){
+    $("input").click(function () {
         var hasPlaceholder = $(this).attr("placeholder");
-        if(hasPlaceholder){
-            $(this).attr("placeholder","");
-            $(this).css("background-color","white");
+        if (hasPlaceholder) {
+            $(this).attr("placeholder", "");
+            $(this).css("background-color", "white");
         }
-        
+
     });
 
-    function isValid(fieldName){
-        var fieldValue = $("input[name="+fieldName+"]").val();
-        var lettersOnly = new RegExp (/[a-zA-Z]{2}/g);
-        var checkPhoneNumber = new RegExp (/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/);
-        var checkEmail = new RegExp (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
-        if(fieldName === "firstName" || fieldName === "lastName"){
-            if(!lettersOnly.test(fieldValue)){
-                showValidationError(fieldName,"Letters only, min-length = 2");
+    $("#clear-button").click(function(){
+        localStorage.clear();
+        console.log(localStorage);
+    });
+
+    function isValid(fieldName) {
+        var fieldValue = $("input[name=" + fieldName + "]").val();
+        var lettersOnly = new RegExp(/[a-zA-Z]{2}/g);
+        var checkPhoneNumber = new RegExp(/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/);
+        var checkEmail = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
+        if (fieldName === "firstName" || fieldName === "lastName") {
+            if (!lettersOnly.test(fieldValue)) {
+                showValidationError(fieldName, "Letters only, min-length = 2");
+                return;
+            }else{
+                return true;
             }
-        }else if(fieldName === "phoneNumber"){
-            if(!checkPhoneNumber.test(fieldValue)){
-                showValidationError(fieldName,"Invalid phone number");
+        } else if (fieldName === "phoneNumber") {
+            if (!checkPhoneNumber.test(fieldValue)) {
+                showValidationError(fieldName, "Invalid phone number");
+                return;
+            }else{
+                return true;
             }
-        }else if(fieldName === "userEmail"){
-            if(!checkEmail.test(fieldValue)){
-                showValidationError(fieldName,"Invalid email address");
+        } else if (fieldName === "userEmail") {
+            if (!checkEmail.test(fieldValue)) {
+                showValidationError(fieldName, "Invalid email address");
+                return;
+            }else{
+                return true;
             }
-        }else{
-            return;
         }
     }
 
-
-    function showValidationError(fieldName,errorText){
-        var field = $("input[name="+fieldName+"]");
+    function showValidationError(fieldName, errorText) {
+        var field = $("input[name=" + fieldName + "]");
         $(field).val("");
-        $(field).css("background-color","pink");
-        $(field).attr("placeholder",errorText);
+        $(field).css("background-color", "pink");
+        $(field).attr("placeholder", errorText);
     }
 
+    function partier(first, last, email, phone, message, attending) {
+        this.firstName = first;
+        this.lastName = last;
+        this.email = email;
+        this.phoneNumber = phone;
+        this.message = message;
+        this.attending = attending;
+        this.fullName = this.firstName + " " + this.lastName;
+    }
+
+    function savePartier() {
+        var first = $("#first_name").val();
+        var last = $("#last_name").val();
+        var email = $("#user_email").val();
+        var phone = $("#phone_number").val();
+        var message = $("#message").val();
+        var attending = false;
+        if ($("#rsvp-yes").prop("checked")) {
+            attending = true;
+        }
+
+        var newPartier = new partier(first, last, email, phone, message, attending);
+
+        var result = JSON.stringify(newPartier);
+
+        var storage = window.localStorage;
+        storage.setItem(newPartier.fullName,result);
+
+        console.log(storage);
+
+    }
 });
